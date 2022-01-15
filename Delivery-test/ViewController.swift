2edf55a4e020e067a337b8f809d7b501a3705139
Вадим <stiksdev@gmail.com>
2022-01-15 11:07:22 +0300

    Initial Commit

---
+++ Delivery-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delivery-test
+//
+//  Created by Вадим Брацюн on 15.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
